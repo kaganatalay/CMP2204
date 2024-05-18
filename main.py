ceeb7a8b926b44eeb5185dd4f26db105f5229f8e
Kağan Atalay <48818679+kaganatalay@users.noreply.github.com>
2024-05-18 13:17:36 +0300

    Revert "Update main.py"
    This reverts commit 96d548c052b579e2c870361f509ab548619464fc.

--- main.py
+++ main.py
@@ -158,8 +158,8 @@ def initiate_chat():
         
         if is_secure:
             private_key, public_key = generate_dh_key()
-            client_socket.send(public_key)  # Send public key directly without encoding
-            peer_public_key = client_socket.recv(2048)  # Assume peer sends public key in correct format
+            client_socket.send(public_key.encode())
+            peer_public_key = client_socket.recv(2048).decode()
             shared_secret = generate_shared_secret(private_key, peer_public_key)
         
         while True:
@@ -174,7 +174,6 @@ def initiate_chat():
             break  # Exit the loop after sending one message
     else:
         print("\nUser not found or offline\n")
-
 
 def menu():
     while True:
