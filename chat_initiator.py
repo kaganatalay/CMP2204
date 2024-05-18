456a5689d39245537f4dc30bccba80b1dba6846d
Kağan Atalay <48818679+kaganatalay@users.noreply.github.com>
2024-05-18 17:48:03 +0300

    Update chat_initiator.py

--- chat_initiator.py
+++ chat_initiator.py
@@ -1,13 +1,10 @@
+import os
 import socket
 import json
 from datetime import datetime
-from cryptography.hazmat.primitives.asymmetric import dh
-from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat, load_pem_public_key
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
-from peer_discovery import PeerDiscovery
 
 TCP_PORT = 6001
 
@@ -59,26 +56,27 @@ class ChatInitiator:
                 return
             
             print(f"Shared secret key is: {shared_secret}")
+
+            iv = os.urandom(16)  # Initialization vector
+            cipher = Cipher(algorithms.AES(shared_secret), modes.CBC(iv), backend=default_backend())
+            encryptor = cipher.encryptor()
             
-            # encrypted_message = self.encrypt_message(shared_secret, message)
-            # message = json.dumps({"encrypted_message": encrypted_message.hex()})
-            # conn.send(message.encode())
-            # self.log_message(target_ip, "SENT", message)
+            padder = padding.PKCS7(algorithms.AES.block_size).padder()
+            padded_data = padder.update(message.encode()) + padder.finalize()
+            
+            encrypted_message = encryptor.update(padded_data) + encryptor.finalize()
+            payload = json.dumps({"encrypted_message": encrypted_message})
+            conn.send(payload.encode())
+
+            print(f"Message sent: {encrypted_message}")
+
+            self.log_message(target_ip, "SENT", message)  
 
     def unsecure_chat(self, target_ip, message):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((target_ip, TCP_PORT))
             s.send(json.dumps({"unencrypted_message": message}).encode())
             self.log_message(target_ip, "SENT", message)
-
-    def exchange_keys(self, s, private_key):
-        public_key = private_key.public_key()
-        s.send(json.dumps({"key": public_key.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo).hex()}).encode())
-        data = s.recv(1024)
-        message = json.loads(data.decode())
-        peer_public_key_bytes = bytes.fromhex(message["key"])
-        peer_public_key = load_pem_public_key(peer_public_key_bytes, backend=default_backend())
-        return peer_public_key
 
     def log_message(self, target_ip, direction, message):
         with open("chat_history.log", "a") as log_file:
